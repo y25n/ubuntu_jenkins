a55d0db8e6988dbb8ac3cefeb1d02cd727a8886b
y25n <cksals0513@naver.com>
2023-06-23 17:55:47 +0900

    gradle set

---
+++ src/main/java/com/ubuntu_jenkins/ubuntu_jenkins/UbuntuJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.ubuntu_jenkins.ubuntu_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbuntuJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbuntuJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ubuntu_jenkins/ubuntu_jenkins/UbuntuJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubuntu_jenkins.ubuntu_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbuntuJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
